cdc3379c90d274b86189fd4821b8632d2d8ecab6
Sam Soffes <sam@soff.es>
2016-05-20 13:27:58 -0700

    Refactor to support bases less than 32 internally

--- Sources/Base62/Base62.swift
+++ Sources/Base62/Base62.swift
@@ -8,7 +8,7 @@
 
 import Darwin
 
-private let alphabet = [
+let base62Alphabet = [
 	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
@@ -16,22 +16,37 @@ private let alphabet = [
 	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 ]
 
-private let base = UInt(alphabet.count)
 
-public func encode(integer integer: UInt) -> String {
+// MARK: - Encoding
+
+public func encode(integer integer: UInt64) -> String {
+	return encode(integer: integer, alphabet: base62Alphabet)
+}
+
+func encode(integer integer: UInt64, alphabet: [String]) -> String {
+	let base = UInt64(alphabet.count)
+
 	if integer < base {
 		return alphabet[Int(integer)]
 	}
-	return encode(integer: integer / base) + alphabet[Int(integer % base)]
+	return encode(integer: integer / base, alphabet: alphabet) + alphabet[Int(integer % base)]
 }
 
-public func decode(string string: String) -> UInt {
-	var output: UInt = 0
+
+// MARK: - Decoding
+
+public func decode(string string: String) -> UInt64 {
+	return decode(string: string, alphabet: base62Alphabet)
+}
+
+func decode(string string: String, alphabet: [String]) -> UInt64 {
+	let base = Double(alphabet.count)
+	var output: UInt64 = 0
 
 	for (i, character) in string.characters.reverse().enumerate() {
 		guard let index = alphabet.indexOf(String(character)) else { continue }
-		let place = UInt(pow(Double(base), Double(i)))
-		output += UInt(index) * place
+		let place = UInt64(pow(base, Double(i)))
+		output += UInt64(index) * place
 	}
 
 	return output
